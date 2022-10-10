1b9cb1395ea336ff59b9bd34b8d7d4d836e6d62e
moschidisg <mosxidismosxidis@gmail.com>
2022-10-10 21:56:32 +0300

    implement new modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import { getForecast } from './apistuff';
+import { setupPage } from './domstuff';
+import { getUserLocation } from './utils';
 
-getForecast('Thessaloniki');
+//DEFAULT LOAD BASED ON LOCATION
+getUserLocation().then((defaultCity) => {
+    citySelected(defaultCity);
+});
+
+async function citySelected(city) {
+    const data = await getForecast(city);
+    setupPage(data);
+}
+
+export { citySelected };
