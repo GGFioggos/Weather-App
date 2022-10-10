2d0ac355aab273ef2df4773e16e1c31622975f2e
moschidisg <mosxidismosxidis@gmail.com>
2022-10-10 21:59:09 +0300

    removed logs and added city as object variable

--- src/apistuff.js
+++ src/apistuff.js
@@ -5,14 +5,12 @@ const API_KEY_ONE = '20f7632ffc2c022654e4093c6947b4f4';
 
 async function getForecast(city) {
     const coords = await getCoords(city);
-
     const response = await fetch(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${API_KEY_ONE}`
     );
     const data = await response.json();
-    const processedData = processData(data);
-    console.log(processedData);
-    return processedData;
+    data.city = city;
+    return processData(data);
 }
 
 export { getForecast };
